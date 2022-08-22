22e785f693e0f7d751f612c062c95f86b1bdf9a0
Tamanna chowdhury <tamannachowdhury399@gmail.com>
2022-08-22 17:56:24 +0600

    third commit

---
+++ js/addname.js
@@ -0,0 +1,23 @@
+document.getElementById('button-name').addEventListener('click', function () {
+    const messiName = document.getElementById('player-One');
+    const messiNameAdd = messiName.innerText;
+    console.log(messiNameAdd);
+
+    // const messiName = document.getElementById('player-One');
+    // const messiNameAdd = messiName.innerText;
+    // console.log(messiNameAdd);
+    // const messiName = document.getElementById('player-One');
+    // const messiNameAdd = messiName.innerText;
+    // console.log(messiNameAdd);
+    // const messiName = document.getElementById('player-One');
+    // const messiNameAdd = messiName.innerText;
+    // console.log(messiNameAdd);
+
+})
+
+document.getElementById('nymar-name').addEventListener('click', function () {
+    const neymarJrName = document.getElementById('player-two');
+    const neymarJrNameAdd = neymarJrName.innerText;
+    console.log(neymarJrNameAdd);
+
+})
